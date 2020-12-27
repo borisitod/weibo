a0f1b13e692311db9ad2d1ebf1a9d231353a490f
Boris <chenjunchi414@gmail.com>
2020-12-27 23:06:41 +0000

    Cutting head and tail subviews

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    Practice deliberately and diligently every day
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >About us</a>
+  </div>
+</footer>
