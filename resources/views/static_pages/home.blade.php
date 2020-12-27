dc9776e959308d3ffcc73b01b70b7fbd98965159
vagrant <vagrant@homestead>
2020-12-27 22:56:50 +0000

    initial styling

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>Home</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      You are viewing the homepage of <a href="https://learnku.com/courses/laravel-essential-training">Laravel Basic</a>.
+    </p>
+    <p>
+      Everything starts from here.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Register now</a>
+    </p>
+  </div>
 @stop
