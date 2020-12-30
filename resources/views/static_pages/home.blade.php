a04f0284d0b2239ed8abe55173edf96d225061c4
Boris <chenjunchi414@gmail.com>
2020-12-30 11:14:23 +0000

    Social statistics

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -15,19 +15,22 @@
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
+        <section class="stats mt-2">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
       </aside>
     </div>
   @else
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        You are viewing the sample homepage of <a href="https://learnku.com/courses/laravel-essential-training">Laravel basic</a>.
+        You are viewing the sample page of <a href="https://learnku.com/courses/laravel-essential-training">Laravel basic</a>.
       </p>
       <p>
         Everything will start here.
       </p>
       <p>
-        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Register now</a>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Register</a>
       </p>
     </div>
   @endif
